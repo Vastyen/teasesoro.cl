1a6ed91ca562c5057dcf442159bfc6b32fac7214
Bastián Escribano <bastian.escribano@usach.cl>
2022-10-12 23:38:27 -0300

    Feat: build

---
+++ build/precache-manifest.7fe84e5dbd7cef23763845857caf7174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5917eae193f151b4041f",
+    "url": "/static/css/main.a7bd840e.chunk.css"
+  },
+  {
+    "revision": "5917eae193f151b4041f",
+    "url": "/static/js/main.8e50a24d.chunk.js"
+  },
+  {
+    "revision": "b7a25d1fe20cdbde6bdd",
+    "url": "/static/js/runtime~main.685209d6.js"
+  },
+  {
+    "revision": "452627c0ffdcfd3252b4",
+    "url": "/static/js/2.97906380.chunk.js"
+  },
+  {
+    "revision": "e0043e67ed505d19b59d",
+    "url": "/static/js/3.29df5f9f.chunk.js"
+  },
+  {
+    "revision": "f041177effde5d81a3e7b6f522484bdd",
+    "url": "/index.html"
+  }
+];
